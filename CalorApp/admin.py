07f99324f57981c51666ada8bcfe0ff9bf91d9e0
Kerriea-star <ojared511@gmail.com>
2021-10-12 12:14:00 +0300

    Calorie calculator app

---
+++ CalorApp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+class foodAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Fooditem
+    list_display = ['name']
+    list_filter = ['name']
+
+admin.site.register(Customer)
+admin.site.register(UserFooditem)
+admin.site.register(Category)
+admin.site.register(Fooditem, foodAdmin)
+
+
